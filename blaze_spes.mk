1d6fb776f557b9ff7a41698e9cb1b413bbc23752
ofcsayan <sayann70alt@gmail.com>
2024-09-25 00:32:00 +0530

    spes: Drop livedisplay [TEST]
    Signed-off-by: ofcsayan <sayann70alt@gmail.com>

--- blaze_spes.mk
+++ blaze_spes.mk
@@ -34,9 +34,5 @@ TARGET_FACE_UNLOCK_SUPPORTED := true
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
-
 # Fingerprint
 BUILD_FINGERPRINT := "Redmi/spes/spes:13/TKQ1.221114.001/V14.0.5.0.TGCINXM:user/release-keys"
